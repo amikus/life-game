60c5cb2e837791300b183e93afe13ea559470585
Andrew Klem <amklem@gmail.com>
2016-05-02 22:07:26 -0400

    Add vector data type for holding coordinate pairs

--- scripts/lifegame.js
+++ scripts/lifegame.js
@@ -1,5 +1,6 @@
 console.log("This will be a game of life.");
 
+/* World map */
 var plan =
     ["############################",
      "#      #    #      o      ##",
@@ -13,3 +14,14 @@ var plan =
      "# o  #         o       ### #",
      "#    #                     #",
      "############################"];
+
+// Vector is used to represent coordinate pairs
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+};
+
