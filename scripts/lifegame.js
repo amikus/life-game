81bddbf6ee4a606fd56316a6bcdf2b54af4cf51b
Andrew Klem <amklem@gmail.com>
2016-05-02 22:18:50 -0400

    Add Grid object with constructor, basic methods, and trivial tests

--- scripts/lifegame.js
+++ scripts/lifegame.js
@@ -1,6 +1,9 @@
 console.log("This will be a game of life.");
 
-/* World map */
+/*********/
+/* World */
+/*********/
+
 var plan =
     ["############################",
      "#      #    #      o      ##",
@@ -21,7 +24,44 @@ function Vector(x, y) {
     this.y = y;
 }
 
+// adds two Vectors
 Vector.prototype.plus = function(other) {
     return new Vector(this.x + other.x, this.y + other.y);
 };
 
+/********/
+/* Grid */
+/********/
+
+// Grid constructor
+function Grid (width, height) {
+    this.space = new Array(width * height);
+    this.width = width;
+    this.height = height;
+}
+
+// determines whether Vector is within boundaires of Grid
+Grid.prototype.isInside function(vector) {
+    return vector.x >= 0 && vector.x < this.width &&
+           vector.y >= 0 && vector.y < this.height;
+};
+
+// gets object located at given Vector
+Grid.prototype.get = function(vector) {
+    return this.space[vector.x + this.width * vector.y];
+};
+
+// places object at given Vector
+Grid.prototype.set = function(vector, value) {
+    this.space[vector.x + this.width * vector.y] = value;
+};
+
+// Grid tests
+
+// result: undefined
+var grid = new Grid(5, 5);
+console.log(grid.get(new Vector(1, 1)));
+
+// result 'X'
+grid.set(new Vector(1, 1), "X");
+console.log(grid.get(new Vector(1, 1)));
